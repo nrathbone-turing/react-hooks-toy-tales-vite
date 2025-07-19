2eac576a40607ac8ef108588247c51b90a3cda60
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-18 19:55:42 -0600

    Create event handlers for change and submit events as controlled inputs and dynamically added to return value output

--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 
-function ToyForm() {
+function ToyForm({ onAddToy }) {
+  
+  const [formData, setFormData] = useState({ name: "", image: "" });
+  
+  function handleChange(event) {
+    const { name, value } = event.target;
+    
+    setFormData({ ...formData, [name]: value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const newToy = { ...formData, likes: 0 };
+    
+    onAddToy(newToy);
+    setFormData({ name: "", image: "" }); // Reset form after submission
+  }
+ 
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
+          value={formData.name}
+          onChange={handleChange}
           placeholder="Enter a toy's name..."
           className="input-text"
         />
@@ -15,6 +34,8 @@ function ToyForm() {
         <input
           type="text"
           name="image"
+          value={formData.image}
+          onChange={handleChange}
           placeholder="Enter a toy's image URL..."
           className="input-text"
         />
