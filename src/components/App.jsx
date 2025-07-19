be8de06d194c6ce23e3718ee77a4bacfcd664469
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-18 19:32:18 -0600

    Refactor App.jsx to include fetchToys function making a GET api call to the server endpoint provided

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,31 @@ import React, { useState } from "react";
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
+import ToyCard from "./ToyCard";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
-
+  const [toys] = useState([]) 
+  const [error, setError] = useState(null)
+  
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
+  
+  const fetchToys = async () => {
+    setError(null)
+    
+    try {
+      const response = await fetch('http://localhost:3001/toys')
+      if (!response.ok) throw new Error('Something went wrong')
+      const toys = await response.json()
+      setData(toys.message)
+    } catch (err) {
+      setError(err.message)
+    }
+  }
+  
+  useEffect(() => fetchToys(), [])
 
   return (
     <>
@@ -18,7 +36,11 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer 
+        toys={toys}
+        onDeleteToy={handleDeleteToy}
+        onLikeToy={handleLikeToy}
+      />
     </>
   );
 }
