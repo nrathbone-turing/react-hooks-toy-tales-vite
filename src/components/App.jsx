fa57821be67c05f64c6c5136ce1d917a9e3ea238
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-18 20:12:15 -0600

    Refactor App.jsx to correctly handle the POST api call

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -7,32 +7,54 @@ import ToyCard from "./ToyCard";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
-  const [toys] = useState([]) 
+  const [toys, setToys] = useState([]) 
   const [error, setError] = useState(null)
   
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
   
+  function handleAddToy(newToy) {
+    // Just used the standard format for any REST API expecting a JSON input
+    fetch("http://localhost:3001/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newToy)
+    })
+    // Extract the JSON body from the response which now includes the unique id for the toy object
+    .then(response => response.json())
+    // Using spread operator to append new toy object to the end of the previous `toys` array
+    // Doing it this way instead of calling `.push()` to make sure the state is updated immutably
+    .then((newToyFromServer) => setToys([...toys, newToyFromServer]))
+    .catch(err => setError(err.message));
+
+  }
+
   const fetchToys = async () => {
     setError(null)
     
     try {
       const response = await fetch('http://localhost:3001/toys')
       if (!response.ok) throw new Error('Something went wrong')
-      const toys = await response.json()
-      setData(toys.message)
+      const data = await response.json()
+      setToys(data)
     } catch (err) {
       setError(err.message)
     }
   }
   
-  useEffect(() => fetchToys(), [])
+  useEffect(() => fetchToys(), []) // starting state of an empty dependency array
+
+  // placeholder variables for now
+  const handleDeleteToy = () => {};
+  const handleLikeToy = () => {};
 
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onAddToy={handleAddToy}/> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
